0d168db26d2f87f4e7e289c9059d029d2f28220a
trungquantrangnguyen <tranntquan@gmail.com>
2024-05-02 18:36:24 +0700

    Added sign in, sign up, sign out, delete user functionalities and tested

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import { PORT } from "./config.js";
 import userRouter from "./routes/user.route.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 mongoose
@@ -16,7 +19,19 @@ mongoose
 
 const app = express();
 
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api/user", userRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
